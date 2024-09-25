d2bdaafa8bf5c09a234e7ae3d8328ec45a6712cc
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-09-25 05:00:02 +0300

    clearer doublelinkedlist

--- algolabra/fringe/doublelinkedlist.py
+++ algolabra/fringe/doublelinkedlist.py
@@ -4,21 +4,13 @@ class Node:
         self.next = next
         self.data = data
 
-
 class DLLIterator:
     def __init__(self, node: Node):
-        self.previous = None
-        self.current = node
+        self.previous = Node(next=node)
 
     def __next__(self):
-        if self.current:
-            self.previous = self.current
-            self.current = self.current.next
-            return self.previous
         if self.previous and self.previous.next:
-            # this happens when handling tail and moving to first of it's children
             self.previous = self.previous.next
-            self.current = self.previous.next
             return self.previous
         raise StopIteration
 
@@ -26,41 +18,38 @@ class DLLIterator:
         return self
 
 class DoubleLinkedList:
-
     def __init__(self, node=None, data=None):
         if data:
             node = Node(data=data)
         self.head = node
-        self.tail = self.head
 
     def add_data_after_node(self, data, node):
         new_node = Node(prev=node, next=node.next, data=data)
         if node.next:
-            node.next.prev = new_node
+            next_node = node.next
+            next_node.prev = new_node
         node.next = new_node
-        if self.tail == node:
-            self.tail = new_node
 
     def remove_node(self, node):
         if node.prev:
             node.prev.next = node.next
         if node.next:
             node.next.prev = node.prev
-        if node == self.head:
+        if self.head == node:
             self.head = node.next
-        if node == self.tail:
-            self.tail = node.prev
 
     def remove_data(self, data):
         node = self.find_node_with_data(data)
-        self.remove_node(node)
+        if node:
+            self.remove_node(node)
 
     def find_node_with_data(self, data):
-        current = self.head
-        while current is not None:
-            if current.data == data:
-                return current
-            current = current.next
+        for node in iter(self):
+            if node.data == data:
+                return node
 
     def __iter__(self):
         return DLLIterator(self.head)
+
+if __name__=='__main__':
+    dll = DoubleLinkedList(data=1)
--- algolabra/tests/fringe/doublelinkedlist_test.py
+++ algolabra/tests/fringe/doublelinkedlist_test.py
@@ -10,20 +10,21 @@ class TestDoubleLinkedList(unittest.TestCase):
         constructed = DoubleLinkedList(data=1)
         self.assertEqual(travel_list(constructed), [1])
         self.assertIsNotNone(constructed.head)
-        self.assertIsNotNone(constructed.tail)
-        self.assertEqual(constructed.head, constructed.tail)
+
+    def test_constructor_with_no_data_works(self):
+        constructed = DoubleLinkedList()
+        self.assertEqual(travel_list(constructed), [])
+        self.assertIsNone(constructed.head)
 
     def test_single_insert_works(self):
         head = self.dll.head
         self.dll.add_data_after_node(2, head)
         self.assertEqual([node.data for node in self.dll], [1, 2])
 
-    def test_single_insert_changes_tail_not_head(self):
+    def test_single_insert_does_not_change_head(self):
         head = self.dll.head
         self.dll.add_data_after_node(2, head)
         self.assertEqual(self.dll.head, head)
-        self.assertNotEqual(self.dll.tail, self.dll.head)
-
 
     def test_10_inserts(self):
         [self.dll.add_data_after_node(i, self.dll.head) for i in range(10, 1, -1)]
@@ -34,30 +35,28 @@ class TestDoubleLinkedList(unittest.TestCase):
         [self.dll.remove_node(nro) for nro in self.dll if nro.data == 3]
         self.assertEqual(travel_list(self.dll), [1, 2, 4])
 
-    def test_remove_node_changes_head_and_tail(self):
+    def test_remove_node_on_only_node_changes_head(self):
         [self.dll.remove_node(node) for node in self.dll if node.data == 1]
-        self.assertEqual(self.dll.head, self.dll.tail)
         self.assertIsNone(self.dll.head)
 
-    def test_remove_node_changes_head(self):
+    def test_remove_node_on_head_changes_head(self):
         fill_to_n(4, self.dll)
         [self.dll.remove_node(nro) for nro in self.dll if nro.data == 1]
         self.assertIsNotNone(self.dll.head)
         self.assertEqual(self.dll.head.data, 2)
 
-    def test_remove_node_changes_tail(self):
+    def test_remove_node_not_on_head_does_not_change_head(self):
         fill_to_n(4, self.dll)
         [self.dll.remove_node(nro) for nro in self.dll if nro.data == 4]
-        self.assertIsNotNone(self.dll.tail)
-        self.assertEqual(self.dll.tail.data, 3)
+        self.assertIsNotNone(self.dll.head)
+        self.assertEqual(self.dll.head.data, 1)
 
-    def test_iterator_works_when_handling_tail_and_adding_children(self):
+    def test_iterator_order_works_when_adding_children_to_working_node(self):
         for node in self.dll:
             if node.data == 3:
                 break
             self.dll.add_data_after_node(node.data + 1, node)
         self.assertEqual(travel_list(self.dll), [1, 2, 3])
-        self.assertEqual(self.dll.tail.data, 3)
 
     def test_find_node_with_data_finds_existing_data(self):
         fill_to_n(4, self.dll)
