06095ce8673ae9f992b48c399ac6c33a994ee298
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-09-28 13:23:21 +0300

    starting to move fringe to octile map

--- algolabra/fringe/doublelinkedlist.py
+++ algolabra/fringe/doublelinkedlist.py
@@ -1,8 +1,11 @@
 class Node:
-    def __init__(self, data=None, prev=None, next=None):
+    def __init__(self, prev=None, next=None, x=None, y=None):
         self.prev = prev
         self.next = next
-        self.data = data
+
+        self.x = x
+        self.y = y
+
 
 class DLLIterator:
     def __init__(self, node: Node):
@@ -18,19 +21,21 @@ class DLLIterator:
         return self
 
 class DoubleLinkedList:
-    def __init__(self, node=None, data=None):
-        if data:
-            node = Node(data=data)
+    def __init__(self, x=None, y=None, node=None):
+        if x and y:
+            node = Node(x=x, y=y)
         self.head = node
 
-    def add_data_after_node(self, data, node):
-        new_node = Node(prev=node, next=node.next, data=data)
+    def add_node(self, x, y, node):
+        new_node = Node(prev=node, next=node.next, x=x, y=y)
         if node.next:
             next_node = node.next
             next_node.prev = new_node
         node.next = new_node
 
     def remove_node(self, node):
+        # so the iterator points at node n. we remove n. we get next from iterator.
+        # as long as we don't alter the next of n we should be good.
         if node.prev:
             node.prev.next = node.next
         if node.next:
@@ -38,15 +43,18 @@ class DoubleLinkedList:
         if self.head == node:
             self.head = node.next
 
-    def remove_data(self, data):
-        node = self.find_node_with_data(data)
+    def remove_at_xy(self, x, y):
+        node = self.find_node_at_xy(x, y)
         if node:
             self.remove_node(node)
 
-    def find_node_with_data(self, data):
+    def find_node_at_xy(self, x, y):
         for node in iter(self):
-            if node.data == data:
+            if node.x == x and node.y == y:
                 return node
+    def removal_version(self, x, y):
+        # TODO replace this with a doublelinkedlist function. do a search for (x,y), rm if found.
+        pass
 
     def __iter__(self):
         return DLLIterator(self.head)
--- algolabra/fringe/fringe_search.py
+++ algolabra/fringe/fringe_search.py
@@ -1,47 +1,48 @@
+from codecs import xmlcharrefreplace_errors
+
 from fringe.doublelinkedlist import DoubleLinkedList
 
+# [0, 'Boston_0_512.map', 512, 512, 344, 85, 343, 85, 1.0]
+def children(node):
+    return [(0,0),(1,1),(2,2),(3,3)]
 
-def fringe_search(start: int, goal: int, children:list, preset_flimit=None):
-    """
-    Version with no comments for now.
-
-    :param start:
-    :param goal:
-    :param children:
-    :param preset_flimit:
-    :return:
-    """
-    fringe = DoubleLinkedList(data=start)
-    cache = [None for i in children]
-    cache[start] = (0, None)
-    flimit = preset_flimit or heuristics(start, goal)
+def fringe_search(start, goal, citymap):
+    goaly, goalx = goal
+    fringe = DoubleLinkedList(*start)
+    cache = [[None for a in line] for line in citymap]
+    # cache = [None for i in range(len(map)*)]
+    cache[start[0]][start[1]] = (0, None, None)
+    flimit = heuristics(start, goal)
     found = False
 
     while not found and fringe.head:
         fmin = 1000000
         for node in fringe:
-            g, parent = cache[node.data]
-            f = g + heuristics(node.data, goal)
+            g, parent = cache[node.y][node.x]
+            f = g + heuristics(node, goal)
             if f > flimit:
                 fmin = min(f, fmin)
                 continue
-            if node.data == goal:
+            if node.x == goal.x and node.y == goal.y:
                 print(f"found route with cost {g}")
                 found = True
                 break
-            for child, cost in reversed(children[node.data]):
+
+            for childy, childx, cost in reversed(children(node)):
                 g_child = g + cost
-                if cache[child]:
-                    g_cached, parent = cache[child]
+                if cache[childy][childx]:
+                    g_cached, parent = cache[childy][childx]
                     if g_child >= g_cached:
                         continue
-                if child in fringe:
-                    fringe.remove_data(child)
-                fringe.add_data_after_node(data=child, node=node)
-                cache[child] = (g_child, node.data)
-                if child == goal:
-                    print(f"{child=}: {g_child=}, {node.data=}")
-                    print(f"fringe: {[node.data for node in fringe]}")
+                # TODO replace this with a doublelinkedlist function. do a search, rm if found.
+                # if child in fringe:
+                #     fringe.remove_data(child)
+                fringe.removal_version(childx, childy)
+                fringe.add_node(childx, childy, node=node)
+                cache[childy][childx] = (g_child, node.data)
+                if childx == goalx and childy == goaly:
+                    print(f"{childx=},{childy=}: {g_child=}, {node.data=}")
+                    print(f"fringe: {[(node.x,node.y) for node in fringe]}")
             fringe.remove_node(node)
 
         flimit = fmin
@@ -49,7 +50,7 @@ def fringe_search(start: int, goal: int, children:list, preset_flimit=None):
         route = [goal]
         while route[-1] != start:
             indeksi = route[-1]
-            route.append(cache[indeksi][1])
+            route.append(cache[indeksi])
         print(route)
         return route
 
