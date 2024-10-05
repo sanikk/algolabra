507dce5cd47ff43f04eeb1e6e4afee995e8c69d5
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-10-05 21:49:08 +0300

    ui/ui.py is looking good

--- algolabra/ui/ui.py
+++ algolabra/ui/ui.py
@@ -1,9 +1,7 @@
 from PyQt6.QtWidgets import QTabWidget, QWidget
-import sys
 
 from algolabra.ui.intro_tab import IntroTab
-from algolabra.ui.search_tabs import AstarTab, FringeTab, SearchTab
-from algolabra.ui.map_scene import MapScene
+from algolabra.ui.search_tabs import SearchTab
 
 
 class TabWindow(QTabWidget):
@@ -12,7 +10,7 @@ class TabWindow(QTabWidget):
 
         intro_tab = IntroTab(search_service=search_service)
         self.addTab(intro_tab, 'intro tab')
-        astar_tab = SearchTab('A*', search_service.playbyplay_astar)
+        astar_tab = SearchTab(search_name='A*', search_func=search_service.playbyplay_astar)
         self.addTab(astar_tab, 'A* tab')
-        fringe_tab = SearchTab('Fringe Search', search_service.playbyplay_fringe)
+        fringe_tab = SearchTab(search_name='Fringe Search', search_func=search_service.playbyplay_fringe)
         self.addTab(fringe_tab, 'Fringe search tab')
