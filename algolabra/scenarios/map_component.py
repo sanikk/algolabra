9f0f7cbf40b5bdb93ee069c7a83c9b973f1089f6
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-10-07 01:14:56 +0300

    map component returns map title, height, width

--- algolabra/scenarios/map_component.py
+++ algolabra/scenarios/map_component.py
@@ -1,37 +1,38 @@
-def read_map(filename):
+def read_map(filename) -> tuple[int, int, list]:
     """
     Read movingai-type map file and preprocess it.
 
-    :param filename:
-    :return: map as list of lines
+    :param filename: object to use as path
+    :return: height, width, map as list of lines
     """
     with open(filename) as f:
         lista = f.read().split("\n")
-    # height = int(lista[1].split(" ")[1])
-    # width = int(lista[2].split(" ")[1])
-    return [line for line in lista[4:] if line]
+    height = int(lista[1].split(" ")[1])
+    width = int(lista[2].split(" ")[1])
+    return height, width, [line for line in lista[4:] if line]
 
-def read_scenarios(filename):
+def read_scenarios(filename) -> tuple[str, list]:
     """
     Read movingai-type map scenario file and preprocess it.
 
     :param filename: path to scenario file
-    :return: list of scenarios: [bucket, (start), (goal), ideal_solution]
+    :return: map_title and list of scenarios: [bucket, (start), (goal), ideal_solution]
     """
     with open(filename) as f:
         lista = f.read().split("\n")
     lines = [a.split("\t") for a in lista[1:] if a]
-    map_file = lines[0][1]
-    print(f"{map_file=}")
+    map_title = lines[0][1]
+
     # bucket, map, (start), (goal), ideal_solution
     converted = [[int(a[0]), (int(a[4]), int(a[5])), (int(a[6]), int(a[7])),
                   float(a[8])] for a in lines]
-    return map_file, converted
+    return map_title, converted
 
 
 if __name__=='__main__':
-    height, width, lista = read_map("Boston_0_512.map")
-    scenarios = read_scenarios("Boston_0_512.map.scen")
+    pass
+    # height, width, lista = read_map("Boston_0_512.map")
+    # scenarios = read_scenarios("Boston_0_512.map.scen")
 
 
 
