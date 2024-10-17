df6a38e1f7dbe1808c6e749bf3d14ebc42977776
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-10-17 14:49:40 +0300

    moved the "live" version to fringe_for_livetab. check_solution to get diag,straight counts from paths

---
+++ algolabra/common_search_utils/check_solution.py
@@ -0,0 +1,15 @@
+def handle_path(path):
+    straight = 0
+    diag = 0
+    edellinen = None
+    for i, piste in enumerate(path):
+        if edellinen:
+            x_delta = piste[0] - edellinen[0]
+            y_delta = piste[1] - edellinen[1]
+            if x_delta and y_delta:
+                diag += 1
+            else:
+                straight += 1
+        edellinen = piste
+    steps = len(path)
+    return steps, straight, diag
