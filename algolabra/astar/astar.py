5cd6a18676a3c155d5b56cb7378d5220eea5bc3c
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-10-21 16:59:17 +0300

    cleaning up astar a bit, docstrings

--- algolabra/astar/astar.py
+++ algolabra/astar/astar.py
@@ -1,17 +1,38 @@
 import time
 from decimal import Decimal, getcontext, Rounded, Inexact
 from heapq import heappush, heappop
+
 from algolabra.common_search_utils.heuristics import heuristics_for_tuples as heuristics
 from algolabra.common_search_utils.children import children_with_tuples as children
 
 
-def reconstruct_path(start, goal, came_from):
+def reconstruct_path(start: tuple[int, int], goal: tuple[int, int], came_from: dict):
+    """
+    Reconstructs path found with A* into a route.
+
+    :param start: start node
+    :param goal: goal node
+    :param came_from: dict with routing data
+    :return: route as a list of nodes
+    """
     path = [goal]
     while path[-1] != start:
         path.append(came_from[path[-1]])
     return path
 
-def timed_astar_search(start, goal, citymap):
+def timed_astar_search(start: tuple[int, int], goal: tuple[int, int], citymap: list):
+    """
+    Times an A* run.
+    :param start:
+    :param goal:
+    :param citymap:
+    :return:cost: cost (as Decimal)
+            timers: perf, proc and thread time_deltas
+            route: path found as an ordered list of nodes.
+            rounded: bool if result was rounded, provided by decimal
+            inexact: bool if result was inexact, provided by decimal
+
+    """
     start_times = [time.perf_counter(), time.process_time(), time.thread_time()]
     cost, route, rounded, inexact = astar(start, goal, citymap)
     end_times = [time.perf_counter(), time.process_time(), time.thread_time()]
@@ -19,7 +40,16 @@ def timed_astar_search(start, goal, citymap):
 
     return cost, timers, route, rounded, inexact
 
-def astar(start,goal, citymap):
+def astar(start: tuple[int, int],goal: tuple[int, int], citymap: list) -> tuple[Decimal, list, bool, bool]:
+    """
+    Simple A* using stock heapq.
+    Currently uses same children and heuristics as Fringe.
+
+    :param start:
+    :param goal:
+    :param citymap:
+    :return:
+    """
     # init
     diag_cost = Decimal('1.4142135623730950488')
     heap = []
@@ -45,6 +75,3 @@ def astar(start,goal, citymap):
 
                 fscore = tentative_gscore + heuristics(x, y, *goal, diag_cost)
                 heappush(heap, (fscore, child))
-
-if __name__=='__main__':
-    pass
