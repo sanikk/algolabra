5979b62fe6d01d13d91d3da8ceee8c3848042da3
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-10-26 17:37:59 +0300

    heuristics looks good. cleaned, tested.

--- algolabra/common_search_utils/heuristics.py
+++ algolabra/common_search_utils/heuristics.py
@@ -1,18 +1,5 @@
 from decimal import Decimal
-from algolabra.fringe.doublelinkedlist import Node
 
-
-def heuristics_with_node(node: Node, goalx: int, goaly: int, diff, diag_cost: Decimal) -> Decimal:
-    """
-    This is an obsolete Wrapper function.
-
-    :param node: Node we calculate a value for
-    :param goalx: x coordinate of goal
-    :param goaly: y coordinate of goal
-    :param diag_cost: cost to move diagonally
-    :return: estimate in Decimal
-    """
-    return old_heuristics(node.x, node.y, goalx, goaly, diff)
 
 def old_heuristics(nx:int, ny: int, goalx: int, goaly: int, diff) -> Decimal:
     """
@@ -39,7 +26,23 @@ def old_heuristics(nx:int, ny: int, goalx: int, goaly: int, diff) -> Decimal:
     return max(abs_delta_x, abs_delta_y) + diff * min(abs_delta_x, abs_delta_y)
 
 
-def new_heuristic(nx, ny, gx, gy, other_diff):
+def heuristics(nx, ny, gx, gy, other_diff):
+    """
+    function heuristic(node) =
+        dx = abs(node.x - goal.x)
+        dy = abs(node.y - goal.y)
+        return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
+
+    where D = 1, D2 = sqrt(2) => diff = diag_cost - 2
+    from http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html#diagonal-distance
+
+    :param nx: node x
+    :param ny: node y
+    :param gx: goal x
+    :param gy: goal y
+    :param other_diff: our replacement of sqrt(2) - 2, with these movement costs
+    :return: very good estimate
+    """
     abs_dx = abs(nx - gx)
     abs_dy = abs(ny - gy)
     return abs_dx + abs_dy + other_diff * min(abs_dx, abs_dy)
--- algolabra/tests/common_search_utils/heuristics_test.py
+++ algolabra/tests/common_search_utils/heuristics_test.py
@@ -1,33 +1,35 @@
 import unittest
 from decimal import Decimal
-# from algolabra.fringe.fringe_search import heuristics
-from algolabra.common_search_utils.heuristics import heuristics_with_node
+from algolabra.common_search_utils.heuristics import heuristics
 
 
 class HeuristicsTest(unittest.TestCase):
 
     def setUp(self):
-        self.diag_cost = Decimal('2').sqrt()
+        self.diag_cost = Decimal('1.4142135623730950488')
+        self.diff = self.diag_cost - Decimal(2)
 
     def test_heuristics_gives_right_cost_to_one_move(self):
-        node = Node(x=1, y=1)
+        self.assertEqual(heuristics(1, 1, 0, 1, self.diff), 1)
+        self.assertEqual(heuristics(1, 1, 2, 1, self.diff), 1)
+        self.assertEqual(heuristics(1, 1, 1, 0, self.diff), 1)
+        self.assertEqual(heuristics(1, 1, 1, 2, self.diff), 1)
 
-        self.assertEqual(heuristics_with_node(node, 0, 1, self.diag_cost), 1)
-        self.assertEqual(heuristics_with_node(node, 2, 1, self.diag_cost), 1)
-        self.assertEqual(heuristics_with_node(node, 1, 0, self.diag_cost), 1)
-        self.assertEqual(heuristics_with_node(node, 1, 2, self.diag_cost), 1)
+        self.assertEqual(heuristics(1, 1, 0, 2, self.diff), self.diag_cost)
+        self.assertEqual(heuristics(1, 1, 2, 2, self.diff), self.diag_cost)
+        self.assertEqual(heuristics(1, 1, 2, 0, self.diff), self.diag_cost)
+        self.assertEqual(heuristics(1, 1, 0, 0, self.diff), self.diag_cost)
 
-        diag_cost = Decimal(2).sqrt()
+    def test_heuristics_with_multiples_of_7(self):
+        diff = self.diff
+        diag_cost = self.diag_cost
+        self.assertEqual(heuristics(14, 13, 7, 13, diff), 7)
+        self.assertEqual(heuristics(23,11, 16, 4, diff), Decimal(7) * diag_cost)
+        self.assertEqual(heuristics(53166, 215123, 53159, 215123, diff), 7)
+        self.assertEqual(heuristics(354, 214, 347, 221, diff), Decimal(7) * diag_cost)
 
-        self.assertEqual(heuristics_with_node(node, 0, 2, diag_cost), diag_cost)
-        self.assertEqual(heuristics_with_node(node, 2, 2, diag_cost), diag_cost)
-        self.assertEqual(heuristics_with_node(node, 2, 0, diag_cost), diag_cost)
-        self.assertEqual(heuristics_with_node(node, 0, 0, diag_cost), diag_cost)
+    def test_heuristics_uses_octile_not_euclidean(self):
+        self.assertEqual(heuristics(600,600, 900, 800, self.diff), Decimal(200) * self.diag_cost + Decimal(100) )
+        pass
 
 
-class Node:
-    def __init__(self, x=None, y=None, prev=None, next=None):
-        self.x = x
-        self.y = y
-        self.prev = prev
-        self.next = next
