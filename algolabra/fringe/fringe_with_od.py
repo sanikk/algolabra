bc3a75fde5c3cab98a95a815930125ff5fa5c732
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-10-27 13:00:13 +0200

    faster fringe with extended ordereddict

---
+++ algolabra/fringe/fringe_with_od.py
@@ -0,0 +1,72 @@
+from decimal import Decimal, getcontext, Rounded, Inexact
+
+from algolabra.fringe.ordered_set_version2 import LastUpdatedOrderedDict, Node
+from algolabra.common_search_utils.heuristics import heuristics
+from algolabra.common_search_utils.children import children as children
+
+
+def fringe_search(start: tuple[int, int], goal: tuple[int, int], citymap: list, diag_cost):
+    map_size = len(citymap)
+    diff = diag_cost - Decimal('2')
+
+    start_node = Node(*start, None, None)
+    fringe = LastUpdatedOrderedDict(start, start_node)
+
+    flimit = heuristics(*start, *goal, diff)
+
+    cache = {start: (0, None, flimit)}
+
+    found = False
+    found_cost = 0
+
+    while not found and fringe:
+        fmin = float('inf')
+        # node is a node here
+        fringe_iterator = iter(fringe)
+        for node in fringe_iterator:
+            # print(f"{node=}, x:{node.x}, y:{node.y}")
+            g, parent, f = cache[(node.x, node.y)]
+            if not f:
+                f = g + heuristics(node.x, node.y, *goal, diff)
+                cache[(node.x, node.y)] = (g, parent, f)
+            if f > flimit:
+                fmin = min(f, fmin)
+                continue
+            if node.x == goal[0] and node.y == goal[1]:
+                found = True
+                found_cost = g
+                print(f"found {g}")
+                break
+            # time this reverse
+            for x, y, cost in reversed(children(node.x, node.y, citymap, diag_cost, map_size)):
+                child = (x, y)
+                g_child = g + cost
+                if child in cache:
+                    if g_child >= cache[child][0]:
+                        continue
+                fringe.add_tail(x,y)
+                cache[child] = g_child, (node.x, node.y), None
+            fringe.remove_by_node(node)
+        flimit = fmin
+
+
+    if found:
+        route = [goal]
+        while route[-1] != start:
+            loc = route[-1]
+            route.append(cache[loc][1])
+        rounded = getcontext().flags[Rounded]
+        inexact = getcontext().flags[Inexact]
+        return found_cost, route, rounded, inexact
+
+if __name__=='__main__':
+    from read_files import read_map
+    le_map = read_map("Boston_0_512.map")
+    diag_cost = Decimal('1.4142135623730950488')
+    # 351	359	354	357	3.82842712
+    # fringe_search((351, 359), (354, 357), le_map, diag_cost)
+    # 405	67	404	64	3.41421356
+    # 10	Boston_0_512.map	512	512	6	409	39	388	43.35533905
+    fringe_search((6, 409), (39, 388), le_map, diag_cost)
+    # 10	Boston_0_512.map	512	512	2	448	21	482	41.87005768
+    # 10	Boston_0_512.map	512	512	67	484	33	504	42.28427124
---
+++ algolabra/fringe/ordered_set_version2.py
@@ -0,0 +1,73 @@
+from collections import OrderedDict
+
+class Node:
+    def __init__(self, x, y, left, right):
+        self.x = x
+        self.y = y
+        self.left = left
+        self.right = right
+
+class LastUpdatedOrderedDict(OrderedDict):
+    """
+    eli key (x,y)
+    value node
+    """
+    def __init__(self, start, start_node, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self[start] = start_node
+        self.head = start_node or Node(None,None,None,right=start_node)
+        self.tail = start_node
+
+    def add_tail(self, x, y):
+        if (x, y) in self.keys():
+            node = self[(x,y)]
+            self._cut_key_links(node)
+            self.move_to_end((x,y))
+        else:
+            self[(x,y)] = node = Node(x, y, None, None)
+
+        node.left = self.tail
+        self.tail.right = node
+        self.tail = node
+
+
+    def remove_by_node(self, node):
+        """
+        ok this should be O(1) ?
+
+        OrderedDict should access this by key, then use the dll structure underneath to actually remove it.
+        My left,right things are just for iterator, they serve no purpose beyond that.
+
+        """
+        self._cut_key_links(node)
+        del self[(node.x, node.y)]
+
+
+    def _cut_key_links(self, node):
+        if node.right:
+            node.right.left = node.left
+        if node.left:
+            node.left.right = node.right
+
+        if self.head == node:
+            self.head = node.right
+        if self.tail == node:
+            self.tail = node.left
+
+    def __iter__(self):
+        return LastUpdatedOrderedDictIterator(self)
+
+class LastUpdatedOrderedDictIterator:
+    def __init__(self, od: LastUpdatedOrderedDict):
+        self.od = od
+        self.current_key = od.head
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_key is None:
+            raise StopIteration
+        prev = self.current_key
+        self.current_key = self.current_key.right
+        return prev
